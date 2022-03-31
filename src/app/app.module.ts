c09652f672e5d06fd22ec5db3ee28e493db99494
George11849 <george11849@gmail.com>
2022-03-31 19:06:18 +0200

    Graph 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PieChartAdvanced1Component } from './pie-chart-advanced1/pie-chart-advanced1.component';
 import { PieChartAdvanced4Component } from './pie-chart-advanced4/pie-chart-advanced4.component';
+import { PieChartAdvanced3Component } from './pie-chart-advanced3/pie-chart-advanced3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     PieChartAdvanced1Component,
+    PieChartAdvanced3Component,
     PieChartAdvanced4Component
   ],
   imports: [
